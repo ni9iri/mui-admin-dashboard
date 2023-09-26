01366034629c8a82d6b99e86c50e722c2b21a52c
ni9iri <quyen.ng1902@gmail.com>
2023-09-26 11:33:59 +0300

    Calendar page added

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import Form from "./scenes/form";
 // import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
-// import Calender from "./scenes/calender";
+import Calendar from "./scenes/calendar";
 
 
 
@@ -40,7 +40,7 @@ function App() {
               {/* <Route path="/pie" element={<Pie />} /> */}
               {/* <Route path="/faq" element={<FAQ />} /> */}
               {/* <Route path="/geography" element={<Geography />} /> */}
-              {/* <Route path="/calender" element={<Calender />} /> */}
+              <Route path="/calendar" element={<Calendar />} />
 
 
 
