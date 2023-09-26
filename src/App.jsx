408a3eb9643fd12190fb2246d8f6c4207136b664
ni9iri <quyen.ng1902@gmail.com>
2023-09-26 15:50:35 +0300

    FAQ page added

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Contacts from "./scenes/contacts";
 import Form from "./scenes/form";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
-// import FAQ from "./scenes/faq";
+import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
 import Calendar from "./scenes/calendar";
 
@@ -38,7 +38,7 @@ function App() {
               <Route path="/form" element={<Form />} />
               {/* <Route path="/line" element={<Line />} /> */}
               {/* <Route path="/pie" element={<Pie />} /> */}
-              {/* <Route path="/faq" element={<FAQ />} /> */}
+              <Route path="/faq" element={<FAQ />} />
               {/* <Route path="/geography" element={<Geography />} /> */}
               <Route path="/calendar" element={<Calendar />} />
 
